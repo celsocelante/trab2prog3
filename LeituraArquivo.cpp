375115693f06fb18c27cd0a993964370412a185f
Celso Junior <celsojr100@gmail.com>
2014-12-06 18:05:14 -0200

    Leitura de arquivos aprimorada

--- LeituraArquivo.cpp
+++ LeituraArquivo.cpp
@@ -1,46 +1,66 @@
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <vector>
-#include <sstream>
-#include <iterator>
+    #include <fstream>
+    #include <iostream>
+    #include <string>
+    #include <cstdlib>
+    #include <vector>
+    #include <sstream>
+    #include <iterator>
+
+    int main()
+    {
+        using namespace std;
+        
+        ifstream inf("artigos.csv");
+     
+        if (!inf) {
+            cerr << "Uh oh, Sample.dat could not be opened for reading!" << endl;
+            exit(1);
+        }
+        
+        string cell, linha;
+
+        string codigo;
+        string titulo;
+        string autores_temp;
+        string contato;
 
 
-int main()
-{
-    using namespace std;
-    
-    ifstream inf("artigos.csv");
- 
-    if (!inf) {
-        cerr << "Uh oh, Sample.dat could not be opened for reading!" << endl;
-        exit(1);
-    }
-    
-    
-    //vector <string> x = split("one:two::three", ':');
-    
-    /*for(int i = 0; i < x.size(); i++)
-      cout << x[i] << endl;*/
-    
-    string linha;
-    string cell;
-    vector <string> x;
-    getline(inf, linha);
-    
-    while (inf) {
-	x.clear();
         getline(inf, linha);
-        //cout << strInput << endl;
-	stringstream lineStream(linha);
+        
+        while (inf) {
+            getline(inf, linha);
+        	stringstream lineStream(linha);
 
-      while(std::getline(lineStream,cell,';')) {
-	x.push_back(cell);
-	cout << cell << endl;
-      }
-      cout << "------" << endl;
+            getline(lineStream,codigo,';');
+
+            getline(lineStream,titulo,';');
+
+            getline(lineStream,autores_temp,';');
+
+            getline(lineStream,contato,';');
+
+            if(contato.empty())
+                // Se o campo contato estiver vazio, quer dizer quer o artigo só possui um autor e ele é o contato
+                contato = autores_temp;
+
+            else {
+                istringstream ss(autores_temp);
+                cout << "Autores: " << endl;
+                while(getline(ss,cell,',')){
+                    if(cell[0] == ' ')
+                        cell.erase(0,0);
+                    cout << cell << endl;
+                    // Adiciona os autores à lista de autores
+                }
+            }
+
+            /* CONSTRUIR OBJETO AQUI */
+
+            
+
+            cout << "Contato: " << contato << endl;
+            cout << "---" << endl;
+        }
+     
+        return 0;
     }
- 
-    return 0;
-}
