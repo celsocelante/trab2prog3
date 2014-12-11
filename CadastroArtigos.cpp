fafb8ed1fbe603b3be40c6fb007fb09ec9d692d1
Celso Junior <celsojr100@gmail.com>
2014-12-11 13:42:05 -0200

    Leitura de arquivos

--- CadastroArtigos.cpp
+++ CadastroArtigos.cpp
@@ -1,1 +1,53 @@
 #include "CadastroArtigos.h"
+
+CadastroArtigos::CadastroArtigos(string entrada, Revista* r){
+  revista = r;
+
+  ifstream inf(entrada);
+
+  /*if (!inf) {
+      cerr << "Uh oh, Sample.dat could not be opened for reading!" << endl;
+      exit(1);
+  }*/
+
+
+  getline(inf, linha);
+
+  while (inf) {
+      getline(inf, linha);
+      stringstream lineStream(linha);
+
+      getline(lineStream,codigo,';');
+
+      getline(lineStream,titulo,';');
+
+      getline(lineStream,autores_temp,';');
+
+      getline(lineStream,contato,';');
+
+      if(contato.empty())
+	  // Se o campo contato estiver vazio, quer dizer quer o artigo só possui um autor e ele é o contato
+	  contato = autores_temp;
+
+      else {
+	  istringstream ss(autores_temp);
+	  cout << "Autores: " << endl;
+	  while(getline(ss,cell,',')){
+	      if(cell[0] == ' ')
+		  cell.erase(0,0);
+		  int autor = atoi(cell.c_str());
+	      cout << autor << endl;
+	      // Adiciona os autores à lista de autores
+	  }
+      }
+
+      /* CONSTRUIR OBJETO AQUI */
+
+      int contato_num = atoi(contato.c_str());
+
+      
+
+      cout << "Contato: " << contato_num << endl;
+      cout << "---" << endl;
+  }
+}
--- CadastroArtigos.h
+++ CadastroArtigos.h
@@ -20,59 +20,13 @@ class CadastroArtigos {
         string titulo;
         string autores_temp;
         string contato;
+	Revista* revista;
+	
         CadastroArtigos(){}
 
     public:
-        CadastroArtigos(string entrada, Revista* revista){
-            ifstream inf(entrada);
- 
-            if (!inf) {
-                cerr << "Uh oh, Sample.dat could not be opened for reading!" << endl;
-                exit(1);
-            }
-
-
-            getline(inf, linha);
-    
-            while (inf) {
-                getline(inf, linha);
-                stringstream lineStream(linha);
-
-                getline(lineStream,codigo,';');
-
-                getline(lineStream,titulo,';');
-
-                getline(lineStream,autores_temp,';');
-
-                getline(lineStream,contato,';');
-
-                if(contato.empty())
-                    // Se o campo contato estiver vazio, quer dizer quer o artigo só possui um autor e ele é o contato
-                    contato = autores_temp;
-
-                else {
-                    istringstream ss(autores_temp);
-                    cout << "Autores: " << endl;
-                    while(getline(ss,cell,',')){
-                        if(cell[0] == ' ')
-                            cell.erase(0,0);
-                            int autor = atoi(cell.c_str());
-                        cout << autor << endl;
-                        // Adiciona os autores à lista de autores
-                    }
-                }
-
-                /* CONSTRUIR OBJETO AQUI */
-
-                int contato_num = atoi(contato.c_str());
-
-                
-
-                cout << "Contato: " << contato_num << endl;
-                cout << "---" << endl;
-            }
-
-        }
+	CadastroArtigos(string entrada, Revista* r);
+        
 };
 
 #endif
