f5d50b2c29d39de3aa9a5dfd66cd3d2fbbb73128
marcosprado <mprado@inf.ufes.br>
2014-12-13 20:48:32 -0200

    Resumo feito. Falta concertar data

--- executa.sh
+++ executa.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ Autor.cpp Artigo.cpp Avaliacao.cpp Revisor.cpp Colaborador.cpp Edicao.cpp Revista.cpp Inconsistencia.cpp Tema.cpp CadastroPessoas.cpp CadastroTemas.cpp CadastroEdicao.cpp CadastroArtigos.cpp CadastroRevisoes.cpp InterpretadorDeArgumentos.cpp Main.cpp -o main
-./main -p entradas/pessoas.csv -r entradas/revisoes.csv -a entradas/artigos.csv -t entradas/temas.csv -e entradas/edicao.txt
+g++ Autor.cpp Artigo.cpp Avaliacao.cpp Revisor.cpp Colaborador.cpp Edicao.cpp Revista.cpp Inconsistencia.cpp Tema.cpp CadastroPessoas.cpp CadastroTemas.cpp CadastroEdicao.cpp CadastroArtigos.cpp CadastroRevisoes.cpp InterpretadorDeArgumentos.cpp RelatorioResumo.cpp Main.cpp -o main
+./main -e entradas/edicao.txt -t entradas/temas.csv -p entradas/pessoas.csv -a entradas/artigos.csv -r entradas/revisoes.csv 
