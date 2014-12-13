d02adc2455e9942b240c630b11d04d5bfb0a1287
Celso Junior <celsojr100@gmail.com>
2014-12-13 18:02:41 -0200

    Arquivos de entrada e script de execucao consertado

--- executa.sh
+++ executa.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 g++ Autor.cpp Artigo.cpp Avaliacao.cpp Revisor.cpp Colaborador.cpp Edicao.cpp Revista.cpp Inconsistencia.cpp Tema.cpp CadastroPessoas.cpp CadastroTemas.cpp CadastroEdicao.cpp CadastroArtigos.cpp CadastroRevisoes.cpp InterpretadorDeArgumentos.cpp Main.cpp -o main
+./main -p entradas/pessoas.csv -r entradas/revisoes.csv -a entradas/artigos.csv -t entradas/temas.csv -e entradas/edicao.txt
