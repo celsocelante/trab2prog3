5a516762ebea44df8701e48570c67c45f88564d4
Marcos Prado <marcosaurelio.mce@gmail.com>
2014-12-14 19:01:26 -0200

    Delete executa.sh

--- executa.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ Autor.cpp Artigo.cpp Avaliacao.cpp Revisor.cpp Colaborador.cpp Edicao.cpp Revista.cpp Inconsistencia.cpp Tema.cpp CadastroPessoas.cpp CadastroTemas.cpp CadastroEdicao.cpp CadastroArtigos.cpp CadastroRevisoes.cpp InterpretadorDeArgumentos.cpp RelatorioResumo.cpp Main.cpp -o main
-./main -e entradas/edicao.txt -t entradas/temas.csv -p entradas/pessoas.csv -a entradas/artigos.csv -r entradas/revisoes.csv 
